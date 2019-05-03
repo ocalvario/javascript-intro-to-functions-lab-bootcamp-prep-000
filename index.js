f47ea97d85978dc687d5532e3da245bf86597e65
Oswald Calvario <oswald.calvario@gmail.com>
2019-05-03 04:51:04 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(shout(string))
+}
+
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+function sayHiToGrandma(string) {
+  var uppercase = string
+  uppercase.toUpperCase() === uppercase
+  console.log("I can\'t hear you!")
+}
