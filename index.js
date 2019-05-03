93aa94110c271d8725751d93f0d906a5f292db43
Oswald Calvario <oswald.calvario@gmail.com>
2019-05-03 05:19:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function logWhisper(string) {
   console.log(whisper(string))
 }
 function sayHiToGrandma(string) {
-  if  {string.toLowerCase === string; 
-  return "I can/'t hear you!"
-  
+  if string.toUpperCase() === string
+  return "YES INDEED!"
 }
